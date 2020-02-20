76783e33b87d0b035acf3cdaaf7ed5bbbca15c36
vaibhav <vaibhavtheory@gmail.com>
2020-02-19 20:17:27 -0800

    Setting up for heroku

--- app.py
+++ app.py
@@ -11,4 +11,6 @@ def handle_submit():
     fname = request.form.get('fname', 'FNAME')
     lname = request.form.get('lname', 'LNAME')
     return render_template('result.html', first_name=fname, last_name=lname)
-    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
