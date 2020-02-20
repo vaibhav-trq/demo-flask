d3214719acaed62a94337f635b0bf4b577f2c67f
vaibhav <vaibhavtheory@gmail.com>
2020-02-19 19:37:03 -0800

    Demo flask project with a form

---
+++ README.md
@@ -0,0 +1,11 @@
+This is an example flask project.
+
+To install:
+```
+pip install -r requirements.txt
+```
+
+To run:
+```
+FLASK_ENV=development FLASK_APP=app.py flask run
+```
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/submit', methods=['POST'])
+def handle_submit():
+    fname = request.form.get('fname', 'FNAME')
+    lname = request.form.get('lname', 'LNAME')
+    return render_template('result.html', first_name=fname, last_name=lname)
+    
